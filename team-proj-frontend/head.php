5dc733043e6f4c64ed6096bb7c4dd2b7e94da531
andrewsylvester117 <andrewsylvester117@gmail.com>
2019-11-20 11:47:58 -0700

    Added .sql file

--- team-proj-frontend/head.php
+++ team-proj-frontend/head.php
@@ -4,7 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo $website." - ".$title; ?></title>
+    <title><?php
+            session_start();
+            echo $_SESSION['sitename']." - ".$title; 
+    
+        ?>
+    </title>
     <link rel = "stylesheet"
         type = "text/css"
         href = "<?php include "getcss.php";?>" />
